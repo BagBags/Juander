79cf71ec42a546647a58e254af2714fef0c0f614
BagBags <aaronbagain@gmail.com>
2025-08-11 23:34:52 +0800

    test 2

--- backend/server.js
+++ backend/server.js
@@ -47,10 +47,10 @@ if (process.env.NODE_ENV === "production") {
   const __dirname = path.resolve();
   app.use(express.static(path.join(__dirname, "frontend", "build")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
-  });
-}
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
+//   });
+// }
 
 // Global error handler
 app.use((err, req, res, next) => {
