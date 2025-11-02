b9156fb1f3ee44ffd9f68213cf97b740eb304741
sophiamikhaelaa <sophiafabian10@gmail.com>
2025-11-02 20:35:36 +0800

    ?
    la naman ako ginagawa dito

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-3e1353d5'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "/index.html",
-    "revision": "0.cmk8qt0ekl8"
+    "revision": "0.0hc9q02f59"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
